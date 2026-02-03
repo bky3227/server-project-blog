c97d0418d2cb49613700a6a0982dae78d6a77913
nong bank <bky3227@gmail.com>
2026-02-03 12:13:32 +0700

    fix cors for dev and prod

--- app.mjs
+++ app.mjs
@@ -6,9 +6,13 @@ const port = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://server-project-blog-o5n4h2ers-banks-projects-f4d0eee4.vercel.app",
+    ],
   })
 );
+
 
 app.use(express.json());
 
